068500b1bde53612e338a618277df4ed321a2bbc
Iqbal <iqbalhossen1418@gmail.com>
2024-02-27 22:29:00 +0600

    after howto change

--- src/pages/howto.js
+++ src/pages/howto.js
@@ -1,49 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '@/styles/howto.module.css';
 import Image from 'next/image';
 import { MdAccessTime } from "react-icons/md";
+import { TfiAngleDown, TfiAngleUp } from "react-icons/tfi";
 import Link from 'next/link';
 
 const HowTo = () => {
+    const [openSummaryIndex, setOpenSummaryIndex] = useState(-1);
+
+    const toggleSummary = (index) => {
+        setOpenSummaryIndex(openSummaryIndex === index ? -1 : index);
+    };
+
     return (
         <>
-
             <div className={styles.container}>
                 <div className={styles.leftSection}>
-
-                    <div className={styles.howToBox}>
-                        <h3>How To Box 1</h3>
-                        <div className={styles.howToContent}>
-                            <div className={styles.howtoimg}>
-                                <Image src="/boximg.jpg" alt="Image 1" width={200} height={200}/>
+                    {[1, 2].map((index) => ( // Assuming you have 5 howToBox elements
+                        <div className={styles.howToBox} key={index}>
+                            <div className={styles.howToContent}>
+                                <div className={styles.howtoimg}>
+                                    <Image src="/boximg2.jpg" className={styles.Image} alt="Image 1" width={250} height={250} />
+                                </div>
+                                <div className={styles.howtotext}>
+                                    <p>ONLYOFFICE DocSpace Brings Seamless Collaboration Within Zoom, and It’s All We Need</p>
+                                    <p>
+                                        <span> DECEMBER 16, 2023</span>
+                                        <span onClick={() => toggleSummary(index)} className={styles.readmorelink}>
+                                            <span>Summary</span> {openSummaryIndex === index ? <TfiAngleUp className={styles.readmore} /> : <TfiAngleDown className={styles.readmore} />}
+                                        </span>
+                                    </p>
+                                </div>
                             </div>
-                            <div className={styles.howtotext}>
-                                <p>Description of the how-to goes here...</p>
-                                <p> DECEMBER 16, 2023</p>
+                            <div style={{ display: openSummaryIndex === index ? 'block' : 'none', padding: '10px' }}>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nihil similique provident quis perspiciatis fugiat quae odio, odit eveniet expedita.</p>
                             </div>
                         </div>
-                    </div>
-                    <div className={styles.howToBox}>
-                        <h3>How To Box 1</h3>
-                        <div className={styles.howToContent}>
-                            <Image src="/boximg.jpg" alt="Image 1" width={200} height={200} />
-                            <p>Description of the how-to goes here...</p>
-                        </div>
-                    </div>
-                    <div className={styles.howToBox}>
-                        <h3>How To Box 1</h3>
-                        <div className={styles.howToContent}>
-                            <Image src="/boximg.jpg" alt="Image 1" width={200} height={200} />
-                            <p>Description of the how-to goes here...</p>
-                        </div>
-                    </div>
-                    <div className={styles.howToBox}>
-                        <h3>How To Box 1</h3>
-                        <div className={styles.howToContent}>
-                            <Image src="/boximg.jpg" alt="Image 1" width={200} height={200} />
-                            <p>Description of the how-to goes here...</p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
                 <div className={styles.rightSection}>
                     <div className={styles.moreoptions}>
